40cb5a719b0ed5bf44c902342c0d6a9d5fa4cf8a
afllm <afllm007@gmail.com>
2014-06-05 09:33:40 +0200

    Alejandro Proyecto para repasar creado.

---
+++ Calculo/src/alx/calculo/Calcular.java
@@ -0,0 +1,27 @@
+package alx.calculo;
+
+public class Calcular {
+	private float num1;
+	private float num2;
+	
+	
+	public float sumar(float numero1, float numero2){
+		this.num1=numero1;
+		this.num2=numero2;
+		
+		return num1+num2;
+	}
+	
+	public float restar(float numero1, float numero2){
+		this.num1=numero1;
+		this.num2=numero2;
+		
+		return num1-num2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
